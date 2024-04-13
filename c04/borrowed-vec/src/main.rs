a857a2bce3a90e42a1a56f41226563083228da90
Brenden Matthews <github@brenden.brndn.io>
2024-04-13 15:33:28 -0400

    cargo fmt

--- c04/borrowed-vec/src/main.rs
+++ c04/borrowed-vec/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let mut vec = vec![1, 2, 3];
     let slice = vec.as_slice();
-    // vec.resize(10, 0); error! does not compile because of the borrow checker
+    // vec.resize(10, 0); error! does not compile because of the borrow
+    // checker
     println!("{}", slice[0]);
 }

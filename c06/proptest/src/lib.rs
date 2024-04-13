d1cb732a23fbd1e957b9838cd7f8b39faf227712
Brenden Matthews <github@brenden.brndn.io>
2024-04-13 15:22:32 -0400

    Expect error for this test

--- c06/proptest/src/lib.rs
+++ c06/proptest/src/lib.rs
@@ -8,6 +8,7 @@ mod tests {
     use proptest::prelude::*;
     proptest! {
         #[test]
+        #[should_panic(expected = "attempt to add with overflow")]
         fn test_add(a: i64, b: i64) {
             assert_eq!(add(a, b), a+b);
         }
